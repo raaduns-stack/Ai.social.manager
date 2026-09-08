1e7bdfb2b41ed77ff5efe229a7bb700e78fcfc3e
jegedeabdulazeez <jegedeabdulazeez123@gmail.com>
2026-09-08 21:15:18 +0100

    designer portal
    ..

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -35,6 +35,7 @@ import { SchedulingModule } from './scheduling/scheduling.module';
 import { PublishingModule } from './publishing/publishing.module';
 import { ChannelsModule } from './channels/channels.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { DesignerModule } from './designer/designer.module';
 
 import * as path from 'path';
 
@@ -87,6 +88,7 @@ import * as path from 'path';
     PublishingModule,
     ChannelsModule,
     NotificationsModule,
+    DesignerModule,
     // Next modules to add here as they're built:
     // UsersModule, ContentCalendarModule, UploadsModule,
     // AiModule (Gemini/OpenClaw integration), NotificationsModule
